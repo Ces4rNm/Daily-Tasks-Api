26cee333adf5fb6bb6fe825a5cd1bd924f4ded3e
Cesar Nuñez <alejoo2310@gmail.com>
2020-10-21 00:51:14 -0500

    Agregando order by fecha

--- Consultas/mostrarTareas.php
+++ Consultas/mostrarTareas.php
@@ -5,11 +5,11 @@
 
     $dbConn = connect($db);
 
-    $sql = $dbConn->prepare("SELECT* FROM tarea");
-      $sql->execute();
-      $sql->setFetchMode(PDO::FETCH_ASSOC);
-      header("HTTP/1.1 200 OK");
-      echo json_encode( $sql->fetchAll()  );
-      exit();
+    $sql = $dbConn->prepare("SELECT * FROM tarea");
+    $sql->execute();
+    $sql->setFetchMode(PDO::FETCH_ASSOC);
+    header("HTTP/1.1 200 OK");
+    echo json_encode( $sql->fetchAll()  );
+    exit();
     
 ?>
