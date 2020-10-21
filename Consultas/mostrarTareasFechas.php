47863ab2d1f586d45befbe265a886e29265f630d
Cesar Nuñez <alejoo2310@gmail.com>
2020-10-21 00:27:47 -0500

    agregando filtro de fecha

---
+++ Consultas/mostrarTareasFechas.php
@@ -0,0 +1,17 @@
+<?php
+
+    include_once '../config.php';
+    include_once '../utils.php';
+
+    $dbConn = connect($db);
+
+    $sql = $dbConn->prepare("SELECT * FROM tarea WHERE fecha > :fechaInicial and fecha < :fechaFinal");
+    $sql->bindValue(':fechaInicial', $_POST['fechaInicial']);
+    $sql->bindValue(':fechaFinal', $_POST['fechaFinal']);
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+    
+?>
