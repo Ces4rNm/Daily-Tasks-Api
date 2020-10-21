40fbd1b6283a9197bb0d8f7d7c46431bdf22e542
Cesar Nuñez <alejoo2310@gmail.com>
2020-10-21 00:52:07 -0500

    Agregando order by fecha

--- Consultas/mostrarTareasFechas.php
+++ Consultas/mostrarTareasFechas.php
@@ -5,13 +5,13 @@
 
     $dbConn = connect($db);
 
-    $sql = $dbConn->prepare("SELECT * FROM tarea WHERE fecha > :fechaInicial and fecha < :fechaFinal");
+    $sql = $dbConn->prepare("SELECT * FROM tarea WHERE fecha > :fechaInicial and fecha < :fechaFinal ORDER BY fecha");
     $sql->bindValue(':fechaInicial', $_POST['fechaInicial']);
     $sql->bindValue(':fechaFinal', $_POST['fechaFinal']);
-      $sql->execute();
-      $sql->setFetchMode(PDO::FETCH_ASSOC);
-      header("HTTP/1.1 200 OK");
-      echo json_encode( $sql->fetchAll()  );
-      exit();
+    $sql->execute();
+    $sql->setFetchMode(PDO::FETCH_ASSOC);
+    header("HTTP/1.1 200 OK");
+    echo json_encode( $sql->fetchAll()  );
+    exit();
     
 ?>
